e75391d26553cd6701f95c9be67e0df57eb1cb05
Matvey <matiaso@bk.ru>
2025-04-01 15:49:02 +0300

    add annotaterb

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: exchanges
+#
+#  id                :integer          not null, primary key
+#  amount_sbtc       :decimal(, )
+#  amount_usdt       :decimal(, )
+#  email             :string
+#  exchange_fee      :decimal(, )
+#  network_fee       :decimal(, )
+#  recipient_address :string
+#  status            :string
+#  txid              :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
 class Exchange < ApplicationRecord
   validates :amount_usdt, presence: true, numericality: { less_than_or_equal_to: 30 }
   validates :recipient_address, presence: true
--- test/models/exchange_test.rb
+++ test/models/exchange_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: exchanges
+#
+#  id                :integer          not null, primary key
+#  amount_sbtc       :decimal(, )
+#  amount_usdt       :decimal(, )
+#  email             :string
+#  exchange_fee      :decimal(, )
+#  network_fee       :decimal(, )
+#  recipient_address :string
+#  status            :string
+#  txid              :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
 require "test_helper"
 
 class ExchangeTest < ActiveSupport::TestCase
